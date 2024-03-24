53b1026f9b8e4699fdd6e69283c3e980c09f8af0
Manal <manal.hebbal@student.vinci.be>
2024-03-24 15:22:45 +0100

    refactor

--- src/Graph.java
+++ src/Graph.java
@@ -1,3 +1,218 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+class Graph {
+  private ArrayList<City> cities;
+  private ArrayList<Road> roads;
+
+  private Map<City, Set<Road>> listeDadjacences;
+
+  public Graph(File city, File road) {
+    this.cities = new ArrayList<City>();
+    this.roads = new ArrayList<Road>();
+    this.listeDadjacences = new HashMap<>();
+
+    try (Scanner cityScanner = new Scanner(city)) {
+      while (cityScanner.hasNextLine()) {
+        String line = cityScanner.nextLine();
+        String[] attributCity = line.split(",");
+        int id = Integer.parseInt(attributCity[0]);
+        String nom = attributCity[1];
+        double longitude = Double.parseDouble(attributCity[2]);
+        double latitude = Double.parseDouble(attributCity[3]);
+        cities.add(new City(id, nom, longitude, latitude));
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+
+    try (Scanner roadScanner = new Scanner(road)) {
+      while (roadScanner.hasNextLine()) {
+        String line = roadScanner.nextLine();
+        String[] attributRoad = line.split(",");
+        int cityId1 = Integer.parseInt(attributRoad[0]);
+        int cityId2 = Integer.parseInt(attributRoad[1]);
+        roads.add(new Road(cityId1, cityId2));
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+
+    for (City cityFor : cities) {
+      listeDadjacences.put(cityFor, new HashSet<>());
+    }
+
+    for (Road roadFor: roads) {
+      City city1 = findCityById(roadFor.getIdCitySource());
+      City city2 = findCityById(roadFor.getIdCityDestination());
+      listeDadjacences.get(city1).add(new Road(city1.getId(),city2.getId()));
+      listeDadjacences.get(city2).add(new Road(city2.getId(),city1.getId()));
+    }
+  }
+
+  private City findCityById(int idCitySource) {
+    for (City city: cities) {
+      if (city.getId() == idCitySource) {
+        return city;
+      }
+    }
+    return null;
+  }
+
+  public void calculerItineraireMinimisantNombreRoutes(String depart, String arrivee) {
+    City source = findCityByName(depart);
+    City destination = findCityByName(arrivee);
+
+    if (source == null || destination == null){
+      throw new IllegalArgumentException("Ville de départ ou d'arrivée null");
+    }
+
+    Queue<City> file = new LinkedList<>();
+    Map<City, Integer> visited = new HashMap<>();
+    Map<City,City> previousCity = new HashMap<>();
+    file.add(source);
+    visited.put(source,0);
+
+    while(!file.isEmpty()){
+      City current = file.remove();
+
+      for (Road road: listeDadjacences.get(current)) {
+        City neighbor = findCityById(road.getIdCityDestination());
+
+        if(!visited.containsKey(neighbor)){
+          visited.put(neighbor, visited.get(current)+1);
+          previousCity.put(neighbor, current);
+          file.add(neighbor);
+        }
+      }
+    }
+
+    ArrayList<City> path = new ArrayList<>();
+    City current = destination;
+    while(current!=null){
+      path.add(0,current);
+      current = previousCity.get(current);
+    }
+
+    System.out.println("Trajet de " + depart + " à " + arrivee + ": " + (path.size()-1) + " routes et " + visited.get(destination) + " km");
+    for (int i = 0; i < path.size()-1 ; i++) {
+      City city1 = path.get(i);
+      City city2 = path.get(i+1);
+      System.out.println(city1.getName() + " -> " + city2.getName() + " (" + Util.distance(city1.getLatitude(),city1.getLongitude(), city2.getLatitude(),city2.getLongitude()) + " km)");
+    }
+  }
+
+  private City findCityByName(String name) {
+    for (City city:cities) {
+      if (city.getName().equals(name)){
+        return city;
+      }
+    }
+    return null;
+  }
+
+  public void calculerItineraireMinimisantKm(String depart, String arrivee) {
+    City start = findCityByName(depart);
+    City end = findCityByName(arrivee);
+
+    if (start == null || end == null){
+      throw new IllegalArgumentException("Ville de départ ou d'arrivée null");
+    }
+
+    Map<City, Double> provisional = new HashMap<>();
+    Map<City, Double> finals = new HashMap<>();
+    Map<City,Road> visited = new HashMap<>();
+    Map<City,City> previous = new HashMap<>();
+
+    City current = start;
+    provisional.put(start, 0.0);
+
+    while (!finals.containsKey(end)) {
+      double minDistance = Double.MAX_VALUE;
+      for (City cityFor : provisional.keySet()) {
+        if (provisional.get(cityFor) < minDistance && !finals.containsKey(cityFor)) {
+          minDistance = provisional.get(cityFor);
+          current = cityFor;
+        }
+      }
+      finals.put(current, minDistance);
+
+      for (Road road : listeDadjacences.get(current)) {
+        City nextCity = findCityById(road.getIdCityDestination());
+        double distance = minDistance + Util.distance(current.getLongitude(), current.getLatitude(), nextCity.getLongitude(), nextCity.getLatitude());
+
+        if (!finals.containsKey(nextCity) && (provisional.get(nextCity) == null || distance < provisional.get(nextCity))) {
+          visited.put(nextCity, road);
+          provisional.put(nextCity, distance);
+          previous.put(nextCity, current);
+        }
+      }
+    }
+
+    // Construct the path
+    ArrayList<City> path = new ArrayList<>();
+    City city = end;
+    while (city != null) {
+      path.add(0, city);
+      city = previous.get(city);
+    }
+
+    // Print the path
+    System.out.println("Itinéraire le plus court en kilomètre");
+    System.out.println("Pour aller de " + depart + " à " + arrivee + ", il est possible de trouver un itinéraire de " + finals.get(end) + " km.");
+    System.out.println();
+    System.out.println("Trajet de " + depart + " à " + arrivee + ": " + (path.size() - 1) + " routes et " + finals.get(end) + " kms");
+    for (int i = 0; i < path.size() - 1; i++) {
+      City city1 = path.get(i);
+      City city2 = path.get(i + 1);
+      double distance = Util.distance(city1.getLongitude(), city1.getLatitude(), city2.getLongitude(), city2.getLatitude());
+      System.out.println(city1.getName() + " -> " + city2.getName() + " (" + distance + " km)");
+    }
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Code aya:
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -196,7 +411,7 @@ class Graph {
           distanceBetweenCities) + " km)");
     }
   }
-}
+}*/
 
 
 
